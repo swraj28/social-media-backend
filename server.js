5d0140900b915df35af4a0d0707bd84fb5a3e0df
swraj28 <swarajkant@gmail.com>
2023-02-10 13:39:51 +0530

    adding other features of users and posts

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const morgan = require("morgan");
 
 const userRoute=require('./routes/users');
 const authRoute=require('./routes/auth');
+const postRoute=require('./routes/posts'); 
 
 dotenv.config();
 
@@ -33,6 +34,8 @@ app.get('/',(req,res)=>{
 
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
+app.use("/api/posts",postRoute);
+
 
 
 app.listen(8000, (err) => {
